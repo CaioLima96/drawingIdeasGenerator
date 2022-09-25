99f983f96edaec0f0bdfc7b7d59cca7408b4627e
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-09-24 23:38:29 -0300

    adicionado mais imagens dos animes

--- resource/js/themes/animes.js
+++ resource/js/themes/animes.js
@@ -27,14 +27,56 @@ const bnha = [
 
     {nameEnglish: 'Asui', img: './assets/img/animes/bnha/asui.png', dataTheme:'bnha'},
 
+    {nameEnglish: 'Bakugou', img: './assets/img/animes/bnha/bakugou.png', dataTheme:'bnha'},
+
     {nameEnglish: 'Shigaraki', img: './assets/img/animes/bnha/shigaraki.png', dataTheme:'bnha'},
 
     // {nameEnglish: '', img: './assets/img/animes/bnha/', dataTheme:'bnha'},
 ]
 
+const onePunchMan = [
+
+    {nameEnglish: 'Bang', img: './assets/img/animes/onePunchMan/bang1.png', dataTheme:'opm'},
+    
+    {nameEnglish: 'Boros Armor', img: './assets/img/animes/onePunchMan/borosArmor.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Boros - 2nd Form', img: './assets/img/animes/onePunchMan/boros2ndForm.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Boros Final Form', img: './assets/img/animes/onePunchMan/borosFinalForm.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Deep Sea King', img: './assets/img/animes/onePunchMan/deepSeaKing.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Deep Sea King - True Form', img: './assets/img/animes/onePunchMan/deepSeaKing(trueForm).png', dataTheme:'opm'},
+
+    {nameEnglish: 'Genos', img: './assets/img/animes/onePunchMan/genos.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Kid Saitama', img: './assets/img/animes/onePunchMan/kidSaitama.png', dataTheme:'opm'},
+
+    {nameEnglish: 'King', img: './assets/img/animes/onePunchMan/king.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Mumen Rider', img: './assets/img/animes/onePunchMan/mumenRider.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Saitama', img: './assets/img/animes/onePunchMan/saitama.png', dataTheme:'opm'},
+
+    {nameEnglish: 'Tatsumaki', img: './assets/img/animes/onePunchMan/tatsumaki.png', dataTheme:'opm'},
+
+    // {nameEnglish: '', img: './assets/img/animes/onePunchMan/', dataTheme:'opm'},
+]
+
+const saintSeiya = [
+
+    {nameEnglish: 'Afrodites', img: './assets/img/animes/saintSeiya/afrodites.png', dataTheme:'saintSeiya'},
+
+    {nameEnglish: 'Aiolia', img: './assets/img/animes/saintSeiya/aiolia.png', dataTheme:'saintSeiya'},
+
+    // {nameEnglish: '', img: './assets/img/animes/saintSeiya/', dataTheme:'saintSeiya'},
+]
+
 const allAnimes = [
     ...bleach,
     ...bnha,
+    ...onePunchMan,
+    ...saintSeiya,
 ]
 
 console.log(allAnimes)
