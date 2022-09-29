991f671c95a2cc752066bae7bd851d787eb24a3b
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-09-29 18:56:07 -0300

    feat: versão inicial do modal

---
+++ resource/js/modal.js
@@ -0,0 +1,47 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+function openModal() {
+
+	modal.style.display = "block";
+	document.body.style.overflow = 'hidden'
+
+	console.log('open')
+}
+
+function closeModal () {
+
+	modal.style.display = "none";
+	document.body.style.overflow = ''
+
+	console.log('close')
+
+
+	// Back to top when the modal is closed
+
+	// const scrollY = document.body.style.top;
+	// document.body.style.top = '';
+	// window.scrollTo(0, parseInt(scrollY || '0') * -1);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+
+	if (event.target == modal) {
+
+		closeModal()
+	}
+}
+
+
+
+
+// ==============================================================
+
+
+let ideasList = document.getElementById('random-name')
+let modalContent = document.getElementsByClassName('modalContent')[0]
+
+
+console.log(ideasList)
+console.log(modalContent)
