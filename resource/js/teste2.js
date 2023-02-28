dbac5154b25915f2b12cd9bebf1ec1cc0350913a
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2023-02-28 16:54:04 -0300

    feat: adicionado tds animes no json

--- resource/js/teste2.js
+++ resource/js/teste2.js
@@ -3,57 +3,86 @@
 // const url = 'http://localhost:3000/animes'
 
 
-async function fetchAsyncc () {
-    const url = `https://testeproj-27b0c-default-rtdb.firebaseio.com/all.json`
-    let response = await fetch(url);
-    let data = await response.json();
-    // console.log(data[0])
-    return data[0]
-}
+// async function fetchAsyncc () {
+//     const url = `https://testeproj-27b0c-default-rtdb.firebaseio.com/all.json`
+//     let response = await fetch(url);
+//     let data = await response.json();
+//     // console.log(data[0])
+//     return data[0]
+// }
 
-async function testee(){
-    dados = await fetchAsyncc()
-    dados.forEach(item => {
-        document.getElementById('ideasList').innerHTML += `
-    
-                <li class='generatedIdea' dataTheme='${item.nameEnglish}'>
-                       <p style='margin-bottom: 16px'>${item.nameEnglish} </p>
+// async function testee(){
+//     dados = await fetchAsyncc()
+//     dados.forEach(item => {
+//         document.getElementById('ideasList').innerHTML += `
 
-                    <div class='imgContainer'><img src='${item.img}' alt=${item.nameEnglish}'></div>
-                </li>
-            `
-    })
-}
-testee()
+//                 <li class='generatedIdea' dataTheme='${item.nameEnglish}'>
+//                        <p style='margin-bottom: 16px'>${item.nameEnglish} </p>
 
-let todosTemas = []
+//                     <div class='imgContainer'><img src='${item.img}' alt=${item.nameEnglish}'></div>
+//                 </li>
+//             `
+//     })
+// }
+// testee()
 
 
-async function fetchAsync (tema) {
+async function fetchAsync(tema) {
     let response = await fetch(`https://testeproj-27b0c-default-rtdb.firebaseio.com/all.json`);
     let data = await response.json();
-    console.log(data[tema])
-    return data[tema]
+
+    let allThemes = [
+        ...data[0],
+        ...data[1]
+    ]
+
+    if (tema = 'all') {
+
+        console.log(allThemes)
+        return allThemes
+
+    } else {
+
+        console.log(data[tema])
+        return data[tema]
+
+    }
 }
 
 
-function getThemeValue (theme) {
+function getThemeValue(theme) {
 
     let temaArr;
-    switch(themeSelector.value) {
+    switch (themeSelector.value) {
+
+        case 'allThemes':
+
+            temaArr = 'all'
+
+            break;
 
         case 'animes':
 
-            // temaArr = teste(0)
-            console.log('tema: ' + themeSelector.value)
             temaArr = 0
+
             break
 
         case 'ben10':
 
-            console.log('tema: ' + themeSelector.value)
-            // temaArr = teste(1)
             temaArr = 1
+
+            break
+
+        case 'games':
+
+            temaArr = allGames
+
+            break
+
+        case 'objects':
+
+            temaArr = allObjects
+
             break
     }
 
@@ -66,11 +95,10 @@ function getThemeValue (theme) {
 //     return dados[tema]
 // }
 
+//tirar ou nao o async?
 async function getMultipleRandom(arr, num) {
 
     console.log(arr)
-    // let arrr = await arr
-    // console.log(arrr)
     let shuffled = [...arr].sort(() => 0.5 - Math.random());
 
     console.log(shuffled.slice(0, num))
@@ -92,27 +120,31 @@ async function setRandomTema() {
     console.log(random)
 
     // for (let i = 0; i < random.length; i++){
-        
+
     //     document.getElementById('ideasList').innerHTML += `
-    
+
     //             <li class='generatedIdea' dataTheme='${random[i].nameEnglish}'>
     //                 <p style='margin-bottom: 16px'>${random[i].nameEnglish} </p>
     //                 <div class='imgContainer'><img src='${random[i].img}' alt=${random[i].nameEnglish}'></div>
     //             </li>
     //         `
-    
+
     // }
 
-    for(let item of random) {
+    for (let item of random) {
         document.getElementById('ideasList').innerHTML += `
     
-                <li class='generatedIdea' dataTheme='${item.nameEnglish}'>
-                    <p style='margin-bottom: 16px'>${item.nameEnglish} </p>
-                    <div class='imgContainer'><img src='${item.img}' alt=${item.nameEnglish}'></div>
+                <li class='generatedIdea' dataTheme='${item.dataSubTheme}'>
+                    <p style='margin-bottom: 16px'>${item.name} </p>
+                    <div class='imgContainer'><img src='${item.img}' alt=${item.name}'></div>
                 </li>
             `
-    
+
     }
+
+    btns[0].classList.add('activeMenuBtn')
+    btns[1].classList.remove('activeMenuBtn')
+    gridView()
 }
 
 document.getElementById('generate').addEventListener('click', setRandomTema);
